b74897425a856aca1724103a4f0ce35abf10ec47
kaichi <hellokitty321ds@gmail.com>
2022-01-22 10:53:21 +0800

    hello world

---
+++ hello_world/target/debug/deps/hello_world-327ed879504255a9.d
@@ -0,0 +1,5 @@
+/Users/kai/Workspace/learn-rust/hello_world/target/debug/deps/hello_world-327ed879504255a9: src/main.rs
+
+/Users/kai/Workspace/learn-rust/hello_world/target/debug/deps/hello_world-327ed879504255a9.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/Users/kai/Workspace/learn-rust/hello_world/target/debug/hello_world: /Users/kai/Workspace/learn-rust/hello_world/src/main.rs
